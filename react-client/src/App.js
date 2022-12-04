ecfe762cff486b1f67167b91a6f0ea5e778bfd26
samkim <milk3531@hotmail.com>
2022-12-03 17:44:39 -0800

    Added basic box to react

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -6,17 +6,19 @@ import "./App.css";
 function App() {
   const [data, setData] = useState('');
 
+  
+
   useEffect(() => {
     fetch("/api")
-      .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
   
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className = 'Item-box'> <p>Name</p> <p>Rating</p><p>Price</p> </div>
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
